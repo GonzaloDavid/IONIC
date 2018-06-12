35c5a52d3bbeffb962e5af6afb2df86c7a52967c
GonzaloDavid <davidgonzalomejia@hotmail.com>
2018-06-12 16:00:57 -0500

    Formularios en Ionic

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,20 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import {NgForm} from "@angular/forms";
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-
+  nombre = "Hola";
+  password = "";
   constructor(public navCtrl: NavController) {
 
   }
+  validarFormulario(formulario: NgForm) {
+    console.log(formulario);
+  }
+
 
 }
